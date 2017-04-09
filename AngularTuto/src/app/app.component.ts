72d0670af6d364e24dd47be881b63aff011706c6
Marco <bmarc94@free.fr>
2017-04-10 00:41:37 +0200

    introducing component and @input decorator

--- AngularTuto/src/app/app.component.ts
+++ AngularTuto/src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-export class Hero {
-  id: number;
-  name: string;
-}
-
+import { Hero } from './hero/hero';
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
@@ -16,14 +12,11 @@ const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
-
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-
     <h2>My Heroes</h2>
-
     <ul class="heroes">
       <li *ngFor="let hero of heroes"
         [class.selected]="hero === selectedHero"
@@ -31,15 +24,7 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <hero-detail [hero]="selectedHero"></hero-detail>
   `,
   styles: [`
     .selected {
@@ -91,14 +76,11 @@ const HEROES: Hero[] = [
     }
   `]
 })
-
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
   selectedHero: Hero;
-
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
 }
