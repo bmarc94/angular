2a274c290538ee36e27decb9064aa3f3c4d41a0e
Marco <bmarc94@free.fr>
2017-04-10 21:47:11 +0200

    introducing router. adding HeroService in appModule providers, changing app.component in heroes component

--- AngularTuto/src/app/app.module.ts
+++ AngularTuto/src/app/app.module.ts
@@ -1,19 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero/detail/hero-detail.component';
+import { HeroesComponent } from './hero/heroes.component';
+import { HeroService } from './hero/service/hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
+
